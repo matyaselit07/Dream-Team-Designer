997fd24c1e8008b51a16c1a84f8a5fbee7e7c6cd
CorvusRex1 <folly.matyas@gmail.com>
2026-08-05 22:03:27 +0200

    setting up the page #2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import heroImg from "./assets/hero.png";
+import "./App.css";
+
+import Header from "./components/Header/Header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
+      <Header />
       <section id="center">
         <div className="hero">
           <img src={heroImg} className="base" width="170" height="179" alt="" />
@@ -116,7 +119,7 @@ function App() {
       <div className="ticks"></div>
       <section id="spacer"></section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div>
+      <h1>It works... for now...</h1>
+    </div>
+  );
+}
